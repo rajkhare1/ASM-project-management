66202bb467d57b3bbd8fd4ea407dc96d9674eaf9
RK00333040 <RK00333040@TMIC-LPT013815.TMHaier.ad>
2019-12-06 19:00:11 +0530

    update for form creation and binding pojo with form

---
+++ project-management/src/main/java/com/raj/pma/controllers/ProjectController.java
@@ -0,0 +1,28 @@
+package com.raj.pma.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.raj.pma.entities.Project;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+	@GetMapping("/new")
+	public String displayProjectForm(Model model) {
+		Project aProject = new Project();
+		model.addAttribute("project", aProject);
+		return "new-project";
+	}
+	
+	@PostMapping("/save")
+	public String createProject(Project project, Model model) {
+	//this should handle saving to database...	
+		return "";
+	}
+	
+}
