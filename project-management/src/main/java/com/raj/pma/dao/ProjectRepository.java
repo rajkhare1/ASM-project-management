f319893f19e05711addfb2eceb0b0503609cbc82
rajkhare <rajkhare@192.168.31.81>
2019-12-09 19:27:55 +0530

    Update for project

---
+++ project-management/src/main/java/com/raj/pma/dao/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.raj.pma.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.raj.pma.entities.Project;
+
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+}
