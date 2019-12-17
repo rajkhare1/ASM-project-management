13c9bba8e5edd8196d43cbb8e2fe4abcd0f7e7ac
RK00333040 <RK00333040@TMIC-LPT013815.TMHaier.ad>
2019-12-17 19:36:15 +0530

    update for CrudRepository for Project entity

--- project-management/src/main/java/com/raj/pma/controllers/ProjectController.java
+++ project-management/src/main/java/com/raj/pma/controllers/ProjectController.java
@@ -1,16 +1,21 @@
 package com.raj.pma.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.raj.pma.dao.ProjectRepository;
 import com.raj.pma.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+	
+	@Autowired
+	ProjectRepository proRepo;
 
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
@@ -21,8 +26,10 @@ public class ProjectController {
 	
 	@PostMapping("/save")
 	public String createProject(Project project, Model model) {
-	//this should handle saving to database...	
-		return "";
+		proRepo.save(project);
+		
+		// use the redirect to prevent duplicate submission 
+		return "redirect:/projects/new";
 	}
 	
 }
--- project-management/src/main/java/com/raj/pma/dao/ProjectRepository.java
+++ project-management/src/main/java/com/raj/pma/dao/ProjectRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.raj.pma.entities.Project;
 
-public interface ProjectRepository extends CrudRepository<Project, Long>{
+public interface ProjectRepository extends CrudRepository<Project, Long> {
 
 }
