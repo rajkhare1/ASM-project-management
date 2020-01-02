0780573dc53820beab65de39bdf6a62d0380e52b
rajkhare <rajkhare@192.168.31.81>
2020-01-02 14:28:12 +0530

    update for postgres

--- project-management/src/main/java/com/raj/pma/entities/Employee.java
+++ project-management/src/main/java/com/raj/pma/entities/Employee.java
@@ -16,7 +16,7 @@ import javax.persistence.ManyToMany;
 public class Employee {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator ="employee_seq")
 	private long employeeId;
 	
 	private String firstName;
--- project-management/src/main/java/com/raj/pma/entities/Project.java
+++ project-management/src/main/java/com/raj/pma/entities/Project.java
@@ -17,7 +17,7 @@ import javax.persistence.ManyToMany;
 public class Project {
 	
 	@Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator ="project_seq")
 	private long projectId;
 	private String name;
 
