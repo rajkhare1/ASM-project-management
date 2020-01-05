cb1f900d77cf846b493e791d0db0975d0b8c0a37
rajkhare <rajkhare@192.168.31.81>
2020-01-05 20:06:14 +0530

    update for AWS

--- project-management/src/test/java/com/raj/pma/dao/ProjectRepositoryIntegrationTest.java
+++ project-management/src/test/java/com/raj/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,24 +1,21 @@
-package com.raj.dao;
+package com.raj.pma.dao;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.raj.pma.ProjectManagementApplication;
 import com.raj.pma.dao.ProjectRepository;
 import com.raj.pma.entities.Project;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({ @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,scripts= {"classpath:schema.sql","classpath:data.sql"}),
 	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts= {"classpath:drop.sql"})})
 public class ProjectRepositoryIntegrationTest {
