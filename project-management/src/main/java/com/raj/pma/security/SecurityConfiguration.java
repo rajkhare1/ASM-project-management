e7613ea2516ae1d0ddff74cd40d46daf678f4395
rajkhare <rajkhare@192.168.31.81>
2020-02-03 15:31:00 +0530

    update for authorization

--- project-management/src/main/java/com/raj/pma/security/SecurityConfiguration.java
+++ project-management/src/main/java/com/raj/pma/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.raj.pma.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,16 +17,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("rajkhare1")
-			.password("efforts007")
-			.roles("USER")
-			   .and()
-			   .withUser("richa")
-			   .password("efforts")
-			   .roles("USER");
+				.password("efforts007")
+				.roles("USER")
+			.and()
+			.withUser("richa")
+				.password("efforts")
+				.roles("USER")
+			.and()
+			.withUser("admin")
+			 	.password("adming")
+			   	.roles("ADMIN");
 	}
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/projects/new").hasAnyRole("ADMIN")
+			.antMatchers("/employees/new").hasAnyRole("ADMIN")
+			.antMatchers("/").authenticated().and().formLogin();
+		
 	}
 }
